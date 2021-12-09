4bd7e69b7010b54d64cfc79697d0cf456e9c2c97
Arthur Lage <arthurlage2006@gmail.com>
2021-12-08 23:15:05 -0300

    toast added to login and register verification

--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
-import '../../styles/login-page.css'
+import toast, { Toaster } from "react-hot-toast";
+
+import "../../styles/login-page.css";
 
 import LogoImage from "../../images/logo.png";
 
@@ -11,27 +13,63 @@ function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const [emailRed, setEmailRed] = useState(false);
+  const [passwordRed, setPasswordRed] = useState(false);
+
   const navigate = useNavigate();
 
+  const checkEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
   const handleLogin = async () => {
-    const data = {
-      email: email,
-      password: password,
-    };
-
-    const response = await api.login(data);
-
-    console.log(response);
-
-    if (response.status === 200) {
-      navigate("/");
+    if (!checkEmail(email)) {
+      setEmailRed(true);
+      toast.error("O email precisa seguir o formato: exemplo@exemplo.com", {
+        style: {
+          fontSize: "1.6rem",
+          fontFamily: "Poppins, sans-serif",
+        },
+      });
     } else {
-      alert("Algo deu errado!");
+      setPasswordRed(false);
+    }
+
+    if (password.length < 4) {
+      setPasswordRed(true);
+      toast.error("A senha precisa ter mais de 4 caracteres", {
+        style: {
+          fontSize: "1.6rem",
+          fontFamily: "Poppins, sans-serif",
+        },
+      });
+    } else {
+      setPasswordRed(false);
+    }
+
+    if (checkEmail(email) && password >= 4) {
+      const data = {
+        email: email,
+        password: password,
+      };
+
+      const response = await api.login(data);
+
+      if (response.status === 200) {
+        navigate("/");
+      } else {
+        alert("Algo deu errado!");
+      }
     }
   };
 
   return (
     <div className="loginPage">
+      <Toaster />
       <div className="logo">
         <Link to="/">
           <img src={LogoImage} alt="E-Shop" />
@@ -43,6 +81,7 @@ function LoginPage() {
             type="email"
             id="email"
             value={email}
+            className={emailRed ? "error" : ""}
             onChange={(e) => setEmail(e.target.value)}
             placeholder="Digite seu email"
           />
@@ -52,6 +91,8 @@ function LoginPage() {
             type="password"
             id="password"
             value={password}
+            className={passwordRed ? "error" : ""}
+            minLength="4"
             onChange={(e) => setPassword(e.target.value)}
             placeholder="Digite sua senha"
           />
@@ -60,7 +101,9 @@ function LoginPage() {
           FAZER LOGIN
         </button>
       </form>
-      <Link className="registerLink" to="/register">Ainda não possui uma conta? Crie uma conta</Link>
+      <Link className="registerLink" to="/register">
+        Ainda não possui uma conta? Crie uma conta
+      </Link>
     </div>
   );
 }
--- src/pages/RegisterPage/index.js
+++ src/pages/RegisterPage/index.js
@@ -6,34 +6,86 @@ import "../../styles/register-page.css";
 import LogoImage from "../../images/logo.png";
 
 import api from "../../services/api";
+import toast, { Toaster } from "react-hot-toast";
 
 function RegisterPage() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const [nameRed, setNameRed] = useState(false)
+  const [emailRed, setEmailRed] = useState(false)
+  const [passwordRed, setPasswordRed] = useState(false)
+
+
   const navigate = useNavigate();
 
+
+    const checkEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
   const handleRegister = async () => {
-    const data = {
-      name: name,
-      email: email,
-      password: password,
-    };
-
-    const response = await api.register(data);
-
-    console.log(response);
-
-    if (response.status === 200) {
-      navigate("/");
+    if (!checkEmail(email)) {
+      setEmailRed(true);
+      toast.error("O email precisa seguir o formato: exemplo@exemplo.com", {
+        style: {
+          fontSize: "1.6rem",
+          fontFamily: "Poppins, sans-serif",
+        },
+      });
     } else {
-      alert("Algo deu errado!");
+      setPasswordRed(false);
+    }
+
+    if (password.length < 4) {
+      setPasswordRed(true);
+      toast.error("A senha precisa ter mais de 4 caracteres", {
+        style: {
+          fontSize: "1.6rem",
+          fontFamily: "Poppins, sans-serif",
+        },
+      });
+    } else {
+      setPasswordRed(false);
+    }
+
+    if(name.length <= 0){
+      setNameRed(true)
+      toast.error("Seu nome não pode estar vazio", {
+        style: {
+          fontSize: "1.6rem",
+          fontFamily: "Poppins, sans-serif",
+        },
+      })
+    } else {
+      setNameRed(false)
+    }
+
+    if (checkEmail(email) && password >= 4 && name.length !== 0) {
+      const data = {
+        name: name,
+        email: email,
+        password: password,
+      };
+
+      const response = await api.register(data);
+
+      if (response.status === 200) {
+        navigate("/");
+      } else {
+        alert("Algo deu errado!");
+      }
     }
   };
 
   return (
     <div className="registerPage">
+      <Toaster />
       <div className="logo">
         <Link to="/">
           <img src={LogoImage} alt="E-Shop" />
@@ -45,6 +97,7 @@ function RegisterPage() {
             type="name"
             id="name"
             value={name}
+            className={nameRed ? "error" : ""}
             onChange={(e) => setName(e.target.value)}
             placeholder="Digite seu nome"
           />
@@ -54,6 +107,7 @@ function RegisterPage() {
             type="email"
             id="email"
             value={email}
+            className={emailRed ? "error" : ""}
             onChange={(e) => setEmail(e.target.value)}
             placeholder="Digite seu email"
           />
@@ -63,8 +117,9 @@ function RegisterPage() {
             type="password"
             id="password"
             value={password}
+            className={passwordRed ? "error" : ""}
             onChange={(e) => setPassword(e.target.value)}
-            placeholder="Digite seu senha"
+            placeholder="Digite sua senha"
           />
         </div>
         <button
